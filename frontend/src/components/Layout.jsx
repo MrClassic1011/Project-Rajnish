f01a8bbb9040a4516cd567faec09cdcd9f96f9ed
MrClassic1011 <pravinubale497@gmail.com>
2026-09-09 00:40:58 +0530

    Completed Sidebar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
 import { styles } from "../assets/dummyStyles";
 import Navbar from "./Navbar.jsx";
+import Sidebar from "./Sidebar.jsx";
+import { useState } from "react";
 
 const Layout = ({ onLogout, user }) => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   return (
     <div className={styles.layout.root}>
       <Navbar user={user} onLogout={onLogout} />
+      <Sidebar
+        user={user}
+        isCollapsed={sidebarCollapsed}
+        setIsCollapsed={setSidebarCollapsed}
+      />
     </div>
   );
 };
